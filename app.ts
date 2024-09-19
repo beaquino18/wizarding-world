52a15ad6ceaa13346e7b81068bc40e7ffa22ed77
Bernadette Aquino <bernadette.p.aquino@gmail.com>
2024-09-18 17:47:02 -0700

    prompt sync activated

--- app.ts
+++ app.ts
@@ -1,2 +1,15 @@
 import PromptSync from "prompt-sync";
 const prompt = PromptSync();
+
+
+function welcomeMessage(name: string) {
+    console.log(`Welcome to Wizarding World ${ name }`)
+};
+
+const getInput = () => {
+    const value = prompt('Enter your name: ');
+    return value;
+}
+
+
+welcomeMessage(getInput());
