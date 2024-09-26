5317d430190a2993062f6c96558512d05297bc5a
Bernadette Aquino <bernadette.p.aquino@gmail.com>
2024-09-25 22:04:01 -0700

    complete take-a-penny

--- take-a-penny/take-a-penny.ts
+++ take-a-penny/take-a-penny.ts
@@ -1,7 +1,6 @@
-import { items } from "./items";
+import { items, printItem } from "./items";
 
-
-const startPennies = 3;
+let startPennies = 3;
 
 console.log('Welcome to our Store!');
 
@@ -10,4 +9,29 @@ while (startPennies > 0) {
 
     const randomItem = Math.floor(Math.random() * items.length);
     const boughtItem = items[randomItem];
+
+    printItem(boughtItem);
+
+    const totalPrice = boughtItem.price;
+
+    console.log(`Total price for ${ boughtItem.name }: ${totalPrice.toFixed(2)}`);
+
+    customerAction();
+}
+
+function customerAction() {
+    const actions = ['take', 'leave']; 
+    const action = actions[Math.floor(Math.random() * actions.length)];
+
+    if (action === 'take') {
+        startPennies--;
+        console.log(`Customer takes a penny. Pennies left: ${startPennies}`);
+    } else {
+        startPennies++;
+        console.log(`Customer leaves a penny. Pennies left: ${startPennies}`);
+    }
+
+    if (startPennies === 0) {
+        console.log(`\nNo more pennies left in the jar. Thank you for shopping!\n`);
+    }
 }
